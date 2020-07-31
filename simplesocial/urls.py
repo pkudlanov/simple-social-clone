1d1d95f35d6a35c4ca549a6cd18027160967cb75
Pavel Kudlanov <pkudlanov@yahoo.com>
2020-07-30 23:45:10 -0400

    implemented django-debug-toolbar

--- simplesocial/urls.py
+++ simplesocial/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 from . import views
 
 urlpatterns = [
@@ -27,3 +28,9 @@ urlpatterns = [
     path('posts/', include('posts.urls', namespace='posts')),
     path('groups/', include('groups.urls', namespace='groups')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls))
+    ] + urlpatterns
